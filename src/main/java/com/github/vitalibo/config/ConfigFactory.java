8c2b815616aebc50b06efc045e573d08485fca15
Vitaliy Boyarsky <boyarsky.vitaliy@live.com>
2022-01-03 06:34:56 +0200

    Initial commit

---
+++ src/main/java/com/github/vitalibo/config/ConfigFactory.java
@@ -0,0 +1,14 @@
+package com.github.vitalibo.config;
+
+import com.typesafe.config.Config;
+
+public final class ConfigFactory {
+
+    private ConfigFactory() {
+    }
+
+    public static Config load(String resourceBasename) {
+        return null;
+    }
+
+}
